2d1df098338c274171d465f933285976b76b7114
NiccoloSiciliano <80923261+NiccoloSiciliano@users.noreply.github.com>
2023-05-07 18:56:47 +0200

    Epsilon decay changed,  reward function changed

--- PendulumEnv.py
+++ PendulumEnv.py
@@ -8,7 +8,11 @@ import sys
 
 
 pygame.init()
+file_name = 'q_table.json'
+pygame.display.set_caption(file_name)
 display = pygame.display.set_mode((1200,600))
+
+
 clock = pygame.time.Clock()
 space = pymunk.Space()
 FPS = 50
@@ -21,7 +25,7 @@ class Box():
         if static:
             self.body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
         else:
-            self.body = pymunk.Body()
+            self.body = pymunk.Body(5, 500)
         self.body.position = x, y
         self.width = width
         self.height = height
@@ -39,24 +43,23 @@ class String():
     def __init__(self, body1, attachement):
         self.body1 = body1
         self.body2 = attachement
-        joint = pymunk.PinJoint(self.body1, self.body2)
-        space.add(joint)
+        self.shape= pymunk.PinJoint(self.body1, self.body2)
+        space.add(self.shape)
     def draw(self):
         x1, y1 = self.body1.position
         x2, y2 = self.body2.position
         pygame.draw.line(display,(0,0,0), (int(x1)-xcamera, int(y1)), (int(x2)-xcamera, int(y2)), 2)
 
 class PendulumEnv:
-    def __init__(self, LEARNING_RATE, DISCOUNT, MAX_EPSILON, MIN_EPSILON, DEACY_RATE, Q_TABLE_DIM,EPISODES, ACTION_NUM, base, box, string,space):
+    def __init__(self, LEARNING_RATE, DISCOUNT, MAX_EPSILON, MIN_EPSILON, DEACY_RATE, Q_TABLE_DIM,EPISODES,  base, box, string,space):
         self.LEARNING_RATE = LEARNING_RATE
         self.DISCOUNT = DISCOUNT
         self.MAX_EPSILON = MAX_EPSILON
         self.MIN_EPSILON = MIN_EPSILON
         self.DECAY_RATE = DEACY_RATE
         self.EPISODES = EPISODES
-        self.ACTION_NUM = ACTION_NUM
-        self.MAX_FRAME = 1000
-
+        self.ANGLE_SAMPLES,self.SPEED_SAMPLES, _,self.ACTION_NUM= Q_TABLE_DIM
+        print(self.ANGLE_SAMPLES,self.SPEED_SAMPLES, self.ACTION_NUM)
         self.q_table = np.zeros(Q_TABLE_DIM)
         self.base = base
         self.box = box
@@ -66,33 +69,16 @@ class PendulumEnv:
         self.space = space
         self.space.gravity = (0, 1000)
         self.action = 0
-        self.frame = 0
 
     def get_epsilon(self,alpha):
-        return self.MAX_EPSILON - (self.MAX_EPSILON /self.EPISODES) * alpha
-        #return self.MIN_EPSILON + (self.MAX_EPSILON - self.MIN_EPSILON)*np.exp(-self.DECAY_RATE*(alpha))
+        r = max((self.EPISODES- alpha)/self.EPISODES, 0)
+        return (self.MAX_EPSILON - self.MIN_EPSILON)*r+self.MIN_EPSILON
     
     def get_reward(self):
         alpha = 0.8
-        dir = self.UP_or_DOWN()
-        v = self.action
-        if dir == 0:
-            dir = -1
+        beta = 1-alpha
         angle = self.get_angle()
-        distance_from_x = 0
-        if angle < 90:
-            distance_from_x =  angle//18 + 5
-        elif angle < 270: 
-            distance_from_x = 15 - angle//18
-        elif angle > 270:
-            distance_from_x = angle//18 - 15
-        return alpha*dir + (1-alpha) * distance_from_x/10
-        return alpha * dir + (1-alpha)*((40-np.abs(v))/40)
-        """
-        angle = int(self.get_angle()//18)
-        if self.UP_or_DOWN() == 1 and  not((self.pre_angle >= 4 and self.pre_angle <= 5) and (angle <4 or angle >5)):
-            return 1
-        return -1"""
+        return alpha* np.sin(angle) + beta* (37-self.get_discrete_velocity(self.get_continuos_velocity(self.box.body.velocity)))/37
 
     def UP_or_DOWN(self):
         if self.box.body.position[1] > self.prev_pos[1]:
@@ -118,9 +104,10 @@ class PendulumEnv:
             if x-xo > 0 and y-yo > 0:
                 return 360 - angle
         return from_0_to_360(xbox, ybox, xbase, ybase, np.degrees(np.arctan(mr)))
+
     def get_new_state(self):
         angle = self.get_angle()
-        return int((angle - angle%18)/18), self.get_discrete_velocity(self.get_continuos_velocity(self.box.body.velocity)), self.UP_or_DOWN() 
+        return int(angle//(360/self.ANGLE_SAMPLES)), self.get_discrete_velocity(self.get_continuos_velocity(self.box.body.velocity)), self.UP_or_DOWN() 
 
     def get_continuos_velocity(self, velocity):
         v1, v2 = velocity
@@ -128,7 +115,7 @@ class PendulumEnv:
         
     def get_discrete_velocity(self, velocity):
         MAX_VEL = 620
-        discrete_v = min(velocity, MAX_VEL-1)
+        discrete_v = min(velocity, MAX_VEL)
         if discrete_v <= 10:
             return int(discrete_v)
         elif discrete_v <= 40:
@@ -138,25 +125,22 @@ class PendulumEnv:
         elif discrete_v <= 220:
             return int(23 + (discrete_v-100)//20)
         elif discrete_v <= 420:
-            # TODO Unire  ultimi due casi
             return int(28 + (discrete_v-220)//40)
-        elif discrete_v <= MAX_VEL:
+        else:
             return int(33 + (discrete_v-420)//40)
-        #return int(discrete_v)
-        return int((discrete_v) // np.ceil(MAX_VEL/80))
 
     def episode_status(self):
         state = self.get_new_state()
         
-        if state[0] >= 4 and state[0] <= 5:
+        if state[0] >= (89//(360/self.ANGLE_SAMPLES)) and state[0] <= (91//(360/self.ANGLE_SAMPLES)):
             self.box.color = (0,255,0)
             if (time.time()-self.timer) > 10:
                 return (True, False)
             
-        elif state[0] >= 14 and state[0] <= 15: 
+        elif state[0] >= (269//(360/self.ANGLE_SAMPLES)) and state[0] <= (271//(360/self.ANGLE_SAMPLES)): 
             self.box.color = (255, 0,0)
             self.timer = time.time()
-            return (False, self.frame >= self.MAX_FRAME)
+            return False,True
         else:
             self.timer = time.time()
             self.box.color = (191, 64, 191)
@@ -171,42 +155,39 @@ class PendulumEnv:
  
     def sample_cond(self, i):
         return i == (self.EPISODES -1)
+
     def train(self):
         global xcamera
         global ycamera
         cmd_t = 0
         for episode in range(self.EPISODES):
-            epsilon = self.get_epsilon(episode)
+            space.remove(self.base.shape, self.base.body)
+            space.remove(self.box.shape, self.box.body)
+            space.remove(self.string.shape)
+
+            self.base= Box(600,300, 100, 10, static=True)
+            self.box = Box(580,100, 50, 50, color=(191, 64, 191))
+            self.string = String(self.base.body, self.box.body)
+
             done = False
             render = False
             truncated = False
-            state = (0,0,0)
-            space.remove(self.base.shape, self.base.body)
-            space.remove(self.box.shape, self.box.body)
             xcamera = 0
             ycamera = 300
-            self.base= Box(600,300, 100, 10, static=True)
-            self.box = Box(599,500, 50, 50, color=(191, 64, 191))
-            self.string = String(self.base.body, self.box.body)
-            self.frame = 0
-            print(episode)
+            successes = 0
+            state = (int(self.get_angle()//(360/self.ANGLE_SAMPLES)),0,0)
+            epsilon = self.get_epsilon(episode)
+
+            print("EPISODE: ", episode)
             if self.sample_cond(episode):
                 input("Last episode")
             line = ''
+
             with open('cmd.txt', 'r')as cmd:
                 line = cmd.readline()
                 if line: 
                     if line == 'save' and not(cmd_t == 1):
-                        print('Saving...', cmd_t)
-                        with open('q_table.json','w') as f:
-                            tosave =[]
-                            x,y,z,d = self.q_table.shape
-                            for i in range(x):
-                                for j in range(y):
-                                    for q in range(z):
-                                        for w in range(d):
-                                            tosave.append(self.q_table[i][j][q][w])
-                            json.dump(tosave, f)
+                        self.save_q_table(file_name)
                         cmd_t = 1
                     if line == 'exit':
                         cmd_t= 2
@@ -222,25 +203,33 @@ class PendulumEnv:
                 if np.random.random() > epsilon:
                     action = np.argmax(self.q_table[state[0],state[1],state[2]])
                 else:
+                    
                     action = np.random.randint(0, self.ACTION_NUM)
-                speed = action%(self.ACTION_NUM//2) * 17
+                
+                speed = (action%(self.ACTION_NUM//2)) * 17
+                
                 if action > (self.ACTION_NUM//2):
                     speed = -speed
 
                 reward,new_state, done, truncated = self.step(speed)
-                if done:
-                    print(episode, done)
+    
                 if self.sample_cond(episode) or render:
                     self.render()
+
                 max_future_q = np.max(self.q_table[new_state[0],new_state[1],new_state[2]])
                 current_q = self.q_table[state[0]][state[1]][state[2]][action]
                 new_q = (1-self.LEARNING_RATE) * current_q + self.LEARNING_RATE * (reward + self.DISCOUNT * max_future_q)
                 
                 self.q_table[state[0],state[1],state[2]][action] = new_q
                 state = new_state
-                self.frame += 1
-            print("Frame: ", self.frame)
-        with open('q_table.json','w') as f:
+            if done:
+                successes += 1
+            print("SUCCESS RATE: ", successes/(episode+1))
+
+        self.save_q_table(file_name)
+
+    def save_q_table(self, file):
+        with open(file,'w') as f:
             tosave =[]
             x,y,z,d = self.q_table.shape
             for i in range(x):
@@ -249,7 +238,8 @@ class PendulumEnv:
                         for w in range(d):
                             tosave.append(self.q_table[i][j][q][w])
             json.dump(tosave, f)
-        
+        print('Q_TABLE SAVED.')
+
     def load_q_table(self, file):
         q_list =[]
         with open(file,'r') as f:
@@ -265,25 +255,31 @@ class PendulumEnv:
                         ind += 1
         return q_table
     def simulate(self):
-        input("Start")
-        self.q_table = self.load_q_table('q_table.json')
-        done = False
-        truncated = False
-        state = (0,0,0)
-        self.timer = time.time()
+        input("START")
         space.remove(self.base.shape, self.base.body)
         space.remove(self.box.shape, self.box.body)
+        space.remove(self.string.shape)
+
         self.base= Box(600,300, 100, 10, static=True)
-        self.box = Box(599,100, 50, 50, color=(191, 64, 191))
+        self.box = Box(580,100, 50, 50, color=(191, 64, 191))
         self.string = String(self.base.body, self.box.body)
+
+        self.q_table = self.load_q_table(file_name)
+        state = (int(self.get_angle()//(360/self.ANGLE_SAMPLES)),0,0)
+        truncated = False
+
+        self.string = String(self.base.body, self.box.body)
+
+        self.timer = time.time()
         while  not truncated:
             action = np.argmax(self.q_table[state[0],state[1],state[2]])
-            speed = action%(self.ACTION_NUM//2)
+            speed = action%(self.ACTION_NUM//2)*17
             if action > (self.ACTION_NUM//2):
                 speed = -speed
-            reward,new_state, done, truncated = self.step(speed)
+            _,new_state, _, truncated = self.step(speed)
             self.render()
             state = new_state
+
     def render(self):
         global xcamera
         for event in pygame.event.get():
@@ -307,7 +303,7 @@ if __name__ == "__main__":
     base = Box(600,300, 100, 10, static=True)
     box = Box(550,50, 50, 50, color=(191, 64, 191))
     string = String(base.body, box.body)
-    env = PendulumEnv(LEARNING_RATE = 0.7, DISCOUNT=0.95, MAX_EPSILON=1.0, MIN_EPSILON=0.05, DEACY_RATE=0.0005, Q_TABLE_DIM = (20, 80, 2, 80),EPISODES=20000, ACTION_NUM=80, base=base, box= box, string=string,space=space)
-    env.train()
+    env = PendulumEnv(LEARNING_RATE = 0.7, DISCOUNT=0.95, MAX_EPSILON=1.0, MIN_EPSILON=0.05, DEACY_RATE=0.005, Q_TABLE_DIM = (20, 39, 2, 80),EPISODES=10000, base=base, box= box, string=string,space=space)
+    env.simulate()
     #floor = Box (300, 350, 800,10, static=True)
     
